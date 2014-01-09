3a3dd63a93d973c4f3aba90a6db09e38d5cd8fc7
Mekan <mekantosh@gmail.com>
2014-01-09 20:29:50 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NdemoApp::Application.config.secret_key_base = '56cb6dfc876cbb8135a4e17f5c63c9d21b4a3e08a041346b1b9e9eb8e8d13569cbd2308446f91f002d649883327826ef047be9ec3ba9b8e98711b0277bdc5c5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NdemoApp::Application.config.session_store :cookie_store, key: '_ndemo_app_session'
